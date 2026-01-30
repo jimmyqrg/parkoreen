b2cf97a2082fe66f8cd1d7b99e1889ff7895c768
JQRG <165354267+JimmyQrg@users.noreply.github.com>
2026-01-30 14:02:18 -0800

    fix jump issue

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching and PWA functionality
  */
 
-const CACHE_NAME = 'parkoreen-v9';
+const CACHE_NAME = 'parkoreen-v10';
 const ASSETS_TO_CACHE = [
     '/parkoreen/',
     '/parkoreen/index.html',
