06077537bbbd003e6c70925ebc2b13a54bbc617e
JQRG <165354267+JimmyQrg@users.noreply.github.com>
2026-01-29 18:53:57 -0800

    try to fix signup

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching and PWA functionality
  */
 
-const CACHE_NAME = 'parkoreen-v5';
+const CACHE_NAME = 'parkoreen-v6';
 const ASSETS_TO_CACHE = [
     '/parkoreen/',
     '/parkoreen/index.html',
